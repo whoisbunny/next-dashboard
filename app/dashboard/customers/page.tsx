031f1efb99c72ff008603d825b8ac3939d61412b
Bunny <kamleshrathod2092000@gmail.com>
2024-06-16 19:43:30 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Customerspage = () => {
+  return (
+    <div>Customerspage</div>
+  )
+}
+
+export default Customerspage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Invoicespage = () => {
+  return (
+    <div>Invoicespage</div>
+  )
+}
+
+export default Invoicespage
